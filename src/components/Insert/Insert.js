b9300087e89013b3398bca19ef422cbe23efacd9
sneezingcat191 <huynguyend191@gmail.com>
2018-12-04 15:52:47 +0700

    finish Insert component

--- src/components/Insert/Insert.js
+++ src/components/Insert/Insert.js
@@ -1,40 +1,49 @@
 import React, { Component } from 'react';
 import Axios from 'axios';
+import classes from './Style.module.css';
 
 class Insert extends Component {
 
   state = {
-    insertOneResult: " ",
-    insertOneMillionResult: " ",
+    result: null,
+    loading: false
   }
   
   insertOne = () => {
+    this.setState({loading: true});
     Axios.post('http://localhost:8000/nedb/1')
     .then(result => {
-      this.setState({ insertOneResult: `${result.data} ms`})
+      this.setState({ result: `${result.data} ms`, loading: false})
     })
-    .catch(error => console.log(error))
+    .catch(error => this.setState({loading: false, result: "Something went wrong"}))
   }
 
   insertOneMillion = () => {
+    this.setState({loading: true});
     Axios.post('http://localhost:8000/nedb/bulk/1000000')
     .then(result => {
-      this.setState({ insertOneMillionResult: `${result.data} ms`})
+      this.setState({ result: `${result.data} ms`, loading: false})
     })
-    .catch(error => console.log(error))
+    .catch(error => this.setState({loading: false, result: "Something went wrong"}))
   }
 
   render() {
+    let data = 'Processing...'
+    if(!this.state.loading) {
+      data = this.state.result;
+    }
     return (
-      <div>
-        <div>
-          <button onClick={this.insertOne}>Insert 1</button>
-          <div>{this.state.insertOneResult}</div>
+      <div className={classes.Wrapper}>
+        <div className={classes.Controller}>
+          <div>
+            <button className={classes.Button} onClick={this.insertOne}>Insert 1</button>
+          </div>
+          <div> 
+            <button className={classes.Button} onClick={this.insertOneMillion}>Insert 1M</button>
+          </div>
         </div>
         <div>
-          <button onClick={this.insertOneMillion}>Insert 1000000</button>
-          <div>{this.state.insertOneMillionResult}</div>
-
+          <div className={classes.Result}>{data}</div>
         </div>
       </div>
     );
